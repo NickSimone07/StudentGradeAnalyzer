ad77b92cdce98d2b7e01e35a6356c71a936e7330
NickSimone07 <52671@student.stkevins.vic.edu.au>
2023-06-04 13:44:19 +1000

    Add project files.

---
+++ StudentGradeAnalyzer/Form1.Designer.vb
@@ -0,0 +1,250 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnImport = New System.Windows.Forms.Button()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.lstStudents = New System.Windows.Forms.ListBox()
+        Me.lblLowest = New System.Windows.Forms.Label()
+        Me.lblHighest = New System.Windows.Forms.Label()
+        Me.btnAddStudent = New System.Windows.Forms.Button()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtGrade = New System.Windows.Forms.TextBox()
+        Me.lblAverage = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnImport
+        '
+        Me.btnImport.Location = New System.Drawing.Point(189, 346)
+        Me.btnImport.Name = "btnImport"
+        Me.btnImport.Size = New System.Drawing.Size(264, 67)
+        Me.btnImport.TabIndex = 0
+        Me.btnImport.Text = "Import File"
+        Me.btnImport.UseVisualStyleBackColor = True
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(1104, 681)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(395, 108)
+        Me.btnCalculate.TabIndex = 1
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(1215, 821)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(164, 106)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'lstStudents
+        '
+        Me.lstStudents.FormattingEnabled = True
+        Me.lstStudents.ItemHeight = 37
+        Me.lstStudents.Location = New System.Drawing.Point(117, 424)
+        Me.lstStudents.Name = "lstStudents"
+        Me.lstStudents.Size = New System.Drawing.Size(384, 411)
+        Me.lstStudents.TabIndex = 3
+        '
+        'lblLowest
+        '
+        Me.lblLowest.AutoSize = True
+        Me.lblLowest.ForeColor = System.Drawing.SystemColors.Control
+        Me.lblLowest.Location = New System.Drawing.Point(806, 706)
+        Me.lblLowest.Name = "lblLowest"
+        Me.lblLowest.Size = New System.Drawing.Size(118, 37)
+        Me.lblLowest.TabIndex = 4
+        Me.lblLowest.Text = "Lowest"
+        '
+        'lblHighest
+        '
+        Me.lblHighest.AutoSize = True
+        Me.lblHighest.ForeColor = System.Drawing.SystemColors.Control
+        Me.lblHighest.Location = New System.Drawing.Point(806, 784)
+        Me.lblHighest.Name = "lblHighest"
+        Me.lblHighest.Size = New System.Drawing.Size(125, 37)
+        Me.lblHighest.TabIndex = 5
+        Me.lblHighest.Text = "Highest"
+        '
+        'btnAddStudent
+        '
+        Me.btnAddStudent.Location = New System.Drawing.Point(1104, 423)
+        Me.btnAddStudent.Name = "btnAddStudent"
+        Me.btnAddStudent.Size = New System.Drawing.Size(395, 200)
+        Me.btnAddStudent.TabIndex = 6
+        Me.btnAddStudent.Text = "Add Student (Do this first)"
+        Me.btnAddStudent.UseVisualStyleBackColor = True
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(752, 376)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(291, 44)
+        Me.txtName.TabIndex = 7
+        '
+        'txtGrade
+        '
+        Me.txtGrade.Location = New System.Drawing.Point(752, 489)
+        Me.txtGrade.Name = "txtGrade"
+        Me.txtGrade.Size = New System.Drawing.Size(291, 44)
+        Me.txtGrade.TabIndex = 8
+        '
+        'lblAverage
+        '
+        Me.lblAverage.AutoSize = True
+        Me.lblAverage.ForeColor = System.Drawing.SystemColors.Control
+        Me.lblAverage.Location = New System.Drawing.Point(806, 635)
+        Me.lblAverage.Name = "lblAverage"
+        Me.lblAverage.Size = New System.Drawing.Size(135, 37)
+        Me.lblAverage.TabIndex = 9
+        Me.lblAverage.Text = "Average"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.SystemColors.Control
+        Me.Label1.Location = New System.Drawing.Point(604, 376)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(103, 37)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.SystemColors.Control
+        Me.Label2.Location = New System.Drawing.Point(604, 489)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(106, 37)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Grade"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(660, 31)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(979, 101)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Student Grade Analyzer"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.ForeColor = System.Drawing.SystemColors.Control
+        Me.Label4.Location = New System.Drawing.Point(630, 635)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(135, 37)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Average"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.ForeColor = System.Drawing.SystemColors.Control
+        Me.Label5.Location = New System.Drawing.Point(647, 706)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(118, 37)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Lowest"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.ForeColor = System.Drawing.SystemColors.Control
+        Me.Label6.Location = New System.Drawing.Point(647, 784)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(125, 37)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Highest"
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(1620, 248)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(344, 132)
+        Me.btnSave.TabIndex = 16
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(19.0!, 37.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.SeaGreen
+        Me.ClientSize = New System.Drawing.Size(2926, 1133)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblAverage)
+        Me.Controls.Add(Me.txtGrade)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.btnAddStudent)
+        Me.Controls.Add(Me.lblHighest)
+        Me.Controls.Add(Me.lblLowest)
+        Me.Controls.Add(Me.lstStudents)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.btnImport)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnImport As Button
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents lstStudents As ListBox
+    Friend WithEvents lblLowest As Label
+    Friend WithEvents lblHighest As Label
+    Friend WithEvents btnAddStudent As Button
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtGrade As TextBox
+    Friend WithEvents lblAverage As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents btnSave As Button
+End Class
---
+++ StudentGradeAnalyzer/Form1.vb
@@ -0,0 +1,199 @@
+﻿Imports System.IO
+Imports System.Xml
+
+Public Class Form1
+    ' Define a structure to hold student records
+    Structure Student
+        Public Name As String
+        Public Grade As Double
+    End Structure
+
+    ' Declare a list to store the student records
+    Dim studentList As New List(Of Student)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Set up the initial state of the form
+        ClearForm()
+    End Sub
+
+    Private Sub ClearForm()
+        ' Clear all textboxes and labels
+        txtName.Clear()
+        txtGrade.Clear()
+        lblAverage.Text = ""
+        lblHighest.Text = ""
+        lblLowest.Text = ""
+        lstStudents.Items.Clear()
+    End Sub
+
+    Private Sub btnAddStudent_Click(sender As Object, e As EventArgs) Handles btnAddStudent.Click
+        ' Validate and process the user input
+        If ValidateInput() Then
+            Dim newStudent As Student
+            newStudent.Name = txtName.Text
+            newStudent.Grade = CDbl(txtGrade.Text)
+
+            ' Add the student to the list
+            studentList.Add(newStudent)
+
+            ' Update the student list display
+            lstStudents.Items.Add(newStudent.Name)
+
+            ' Clear the textboxes
+            txtName.Clear()
+            txtGrade.Clear()
+
+            ' Focus on the name textbox
+            txtName.Focus()
+        End If
+    End Sub
+
+    Private Function ValidateInput() As Boolean
+        ' Validate the user input for student name and grade
+        If String.IsNullOrWhiteSpace(txtName.Text) Then
+            MessageBox.Show("Please enter a valid student name.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        End If
+
+        Dim grade As Double
+        If Not Double.TryParse(txtGrade.Text, grade) Then
+            MessageBox.Show("Please enter a valid numeric grade.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        End If
+
+        If grade < 0 Or grade > 100 Then
+            MessageBox.Show("Please enter a valid grade between 0 and 100.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        End If
+
+        Return True
+    End Function
+
+    Private Sub btnImport_Click(sender As Object, e As EventArgs) Handles btnImport.Click
+        ' Import student data from a file
+        Dim openFileDialog As New OpenFileDialog()
+
+        ' Set the filter to XML files
+        openFileDialog.Filter = "XML Files|*.xml"
+
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            Try
+                LoadDataFromXml(openFileDialog.FileName)
+                MessageBox.Show("Data imported successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Catch ex As Exception
+                MessageBox.Show("An error occurred while importing the data. Please try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+    End Sub
+
+    Private Sub LoadDataFromXml(xmlFilePath As String)
+        ' Load data from XML file and populate the studentList
+        Try
+            Dim doc As New XmlDocument()
+            doc.Load(xmlFilePath)
+
+            studentList.Clear()
+            lstStudents.Items.Clear()
+
+            Dim studentNodes As XmlNodeList = doc.SelectNodes("/students/student")
+            For Each studentNode As XmlNode In studentNodes
+                Dim newStudent As Student
+                newStudent.Name = studentNode.SelectSingleNode("name").InnerText
+                newStudent.Grade = CDbl(studentNode.SelectSingleNode("grade").InnerText)
+
+                studentList.Add(newStudent)
+                lstStudents.Items.Add(newStudent.Name)
+            Next
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        ' Calculate statistics based on the student data
+        If studentList.Count > 0 Then
+            ' Calculate average grade
+            Dim totalGrade As Double = 0
+            For Each student In studentList
+                totalGrade += student.Grade
+            Next
+            Dim averageGrade As Double = totalGrade / studentList.Count
+            lblAverage.Text = averageGrade.ToString("F2")
+
+            ' Find highest and lowest grades
+            Dim highestGrade As Double = Double.MinValue
+            Dim lowestGrade As Double = Double.MaxValue
+            For Each student In studentList
+                If student.Grade > highestGrade Then
+                    highestGrade = student.Grade
+                End If
+                If student.Grade < lowestGrade Then
+                    lowestGrade = student.Grade
+                End If
+            Next
+            lblHighest.Text = highestGrade.ToString("F2")
+            lblLowest.Text = lowestGrade.ToString("F2")
+        Else
+            MessageBox.Show("No student records found.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub lstStudents_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstStudents.SelectedIndexChanged
+        ' Display the selected student's grade
+        If lstStudents.SelectedIndex >= 0 Then
+            Dim selectedStudent As Student = studentList(lstStudents.SelectedIndex)
+            MessageBox.Show("Selected student's grade: " & selectedStudent.Grade.ToString("F2"), "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' Clear the form and student list
+        ClearForm()
+        studentList.Clear()
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        ' Save student data to an XML file
+        Dim saveFileDialog As New SaveFileDialog()
+
+        ' Set the filter to XML files
+        saveFileDialog.Filter = "XML Files|*.xml"
+
+        If saveFileDialog.ShowDialog() = DialogResult.OK Then
+            Try
+                SaveDataToXml(saveFileDialog.FileName)
+                MessageBox.Show("Data saved successfully to XML.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Catch ex As Exception
+                MessageBox.Show("An error occurred while saving data to XML. Please try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+    End Sub
+
+    Private Sub SaveDataToXml(xmlFilePath As String)
+        ' Save studentList data to XML file
+        Try
+            Dim doc As New XmlDocument()
+            doc.LoadXml("<students></students>")
+
+            Dim rootElement As XmlElement = doc.DocumentElement
+
+            For Each student As Student In studentList
+                Dim studentElement As XmlElement = doc.CreateElement("student")
+
+                Dim nameElement As XmlElement = doc.CreateElement("name")
+                nameElement.InnerText = student.Name
+                studentElement.AppendChild(nameElement)
+
+                Dim gradeElement As XmlElement = doc.CreateElement("grade")
+                gradeElement.InnerText = student.Grade.ToString()
+                studentElement.AppendChild(gradeElement)
+
+                rootElement.AppendChild(studentElement)
+            Next
+
+            doc.Save(xmlFilePath)
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+End Class
